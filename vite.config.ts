28775d5b9f167b61065fcd3cf42edca39507bdcd
YunTaoYoung <yt@mock.com>
2026-01-06 09:55:29 +0800

    Add dev build configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
-import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 import manifest from './manifest.json';
 
 export default defineConfig({
@@ -11,7 +11,19 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dev',
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
     },
   },
 });
